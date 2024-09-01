c891f5917e87b6277c8bcd364c39bdf2b464361d
Jason Tran <keyotter.jason@gmail.com>
2024-09-01 14:11:52 -0700

    fixed out of range border background, modified page content according to figma design

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -9,38 +9,38 @@ export default function About() {
   return (
     <>
       <Navbar />
-      <div className="text-block">
+      <div className={`text-block`}>
         <h1>ABOUT US</h1>
         <p>
           Established in 2024, Bruin Business is UCLA's newest business
           organization managed by both undergraduate and graduate students. We
-          are open to UCLA undergraduate and graduate students of all
+          are <br /> open to UCLA undergraduate and graduate students of all
           backgrounds and skill levels to join our growing community and share
-          our love for business! <br /> <br /> Feel free to join us on LinkedIn,
+          our love for <br /> business! <br /> <br /> Feel free to join us on LinkedIn,
           Instagram, and Facebook!
         </p>
         <h1>WHAT WE DO</h1>
         <p>
-          At Bruin Business, our mission is to foster a thriving entreprenuerial community through a diverse 
-          range of activities and initiatives. With events such as info sessions, workshops, speaker panels, networking,
-          and startups, we strive to empower our members with a collaborative environment and tools needed to grow. We are
-          open to everyone interested in business and entrepreneurship, regardless of major or experience!
+          At Bruin Business, our mission is to foster a thriving entreprenuerial community through a diverse
+          range of activities and initiatives. With <br /> events such as info sessions, workshops, speaker panels, networking,
+          and startups, we strive to empower our members with a collaborative <br /> environment and tools needed to grow. We are
+          open to everyone interested in business and entrepreneurship, regardless of major or <br /> experience!
         </p>
         <h1>HOW YOU CAN BE INVOLVED</h1>
         <p>
           Regardless of your major or background, we would love to have you at our open events!
-          <br /> <br/>
+          <br /> <br />
           To keep up with our events, feel free to join us through following our newsletter, Instagram, Facebook, and LinkedIn!
         </p>
-        <div style={{backgroundColor: "lightgrey"}}className="container">
-        <p style={{fontSize: "50px"}} className="border">
-         Join Bruin Business Today!
-         <button className="button" onClick={() => navigate('/join-student')}> Join Now </button>
-         <p style={{fontSize: "20px"}}>
-          Become a part of our community and gain valuable skills and experiences.
-         </p>
-         </p>
-         </div>
+        <div className="container"> {/* The interactive forum box; will fix out of bound bg-color; will adjust the border size of container   */}
+          <p style={{ fontWeight: "600" , fontSize: "50px" }} className="border">
+            Join Bruin Business Today!
+            <button className="button" onClick={() => navigate('/join-student')}> Join Now </button>
+            <p style={{ fontFamily: "Noto Sans", fontSize: "20px" }}>
+              Become a part of our community and gain valuable skills and experiences.
+            </p>
+          </p>
+        </div>
       </div>
       <Footer />
     </>
