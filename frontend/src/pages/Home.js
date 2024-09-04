8c87fd0e659401941dde05774117a5f6cfe4b430
Justin Nguyen <justinnguyen2026@gmail.com>
2024-09-03 18:03:52 -0700

    Jason's changes

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="page-container-home">
       <Navbar />
       <div className="overlay-content">
-        <h1 style={{fontSize: "100px"}}>Bruin Business</h1>
+        <h1>Bruin Business</h1>
         <p>Work in Progress. <br/>Developed by Vincent Pham, Justin Nguyen, Jason Tran, and Christopher Nguyen.</p>
       </div> 
       <div className="content-wrap">
