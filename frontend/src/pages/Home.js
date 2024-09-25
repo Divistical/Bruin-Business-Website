5444b53bd897de5f53eab59f635980ce450e2da8
Chris <christophernguyen71@gmail.com>
2024-09-24 22:13:47 -0700

    fixed auth for real

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -36,11 +36,11 @@ export default function Home() {
       <Navbar />
       <div className="overlay-content">
         <h1 style={{ fontSize: "100px" }}>Bruin Business</h1>
-        <p>
+        {isAdmin && <p>
           Work in Progress. <br />
           Developed by Vincent Pham, Justin Nguyen, Jason Tran, and Christopher
           Nguyen.
-        </p>
+        </p>}
       </div>
       <div className="content-wrap">
         {isAdmin && (
